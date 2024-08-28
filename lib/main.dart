839f7c81fadc025fc4eda99a71f7feb05e76b921
Leonardo Araújo <lcostaaraujo9@gmail.com>
2024-08-28 11:54:25 -0300

    inicio

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+
+    Container(
+      color: Colors.amber,
+    ),
+
+/*MaterialApp(
+  color: Color.fromARGB(255, 255, 255, 0),
+  title: "WORKSHOP",
+)
+*/
+  );
+}
